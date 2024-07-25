972f24e6827630808bf4ef5c0ddf0335ddc9687d
diwahar-m <msdiwahar5@gmail.com>
2024-07-25 19:25:06 +0530

    Add to cart functionality is added

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import { useContext } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 
-const FoodItem = ({ foodItem: { id, name, price, description, image } }) => {
+const FoodItem = ({ foodItem: { _id, name, price, description, image } }) => {
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img src={image} alt="" className="food-item-image" />
+        {!cartItems[_id] ? (
+          <img
+            className="add"
+            alt=""
+            src={assets.add_icon_white}
+            onClick={() => addToCart(_id)}
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              alt=""
+              src={assets.remove_icon_red}
+              onClick={() => removeFromCart(_id)}
+            />
+            <p>{cartItems[_id]}</p>
+            <img
+              alt=""
+              src={assets.add_icon_green}
+              onClick={() => addToCart(_id)}
+            />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
