df01c34ae2562114805ec20553aa867735087d20
diwahar-m <msdiwahar5@gmail.com>
2024-07-26 09:00:30 +0530

    Food filters added

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,9 +10,11 @@ const FoodDisplay = ({ category }) => {
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        {food_list.map((item, index) => (
-          <FoodItem key={index} foodItem={item} />
-        ))}
+        {food_list.map((item, index) => {
+          if (category === "All" || category === item.category) {
+            return <FoodItem key={index} foodItem={item} />;
+          }
+        })}
       </div>
     </div>
   );
