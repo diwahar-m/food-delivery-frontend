899a1001bb1edbd1bf174f4269698c5810a557f9
diwahar-m <msdiwahar5@gmail.com>
2024-11-24 16:14:46 +0530

    track order onclick event integrated

--- src/pages/MyOrders/MyOrders.jsx
+++ src/pages/MyOrders/MyOrders.jsx
@@ -45,7 +45,7 @@ const MyOrders = () => {
               <p>
                 <span>&#x25cf</span>
                 <b>{order.status}</b>
-                <button> Track Order</button>
+                <button onClick={fetchOrders}> Track Order</button>
               </p>
             </div>
           );
